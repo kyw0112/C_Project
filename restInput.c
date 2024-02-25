47a4bcaaeb0410d9712a25f1ef9f5f4cf6b0e951
Toughie17 <airj907@gmail.com>
2024-02-25 18:02:33 +0900

    feat: db 중복체크 후 삽입

--- restInput.c
+++ restInput.c
@@ -1,7 +1,5 @@
 //restInput
 
-// 디비 중복 확인
-
 #include <sqlite3.h>
 #include <stdio.h>
 #include <string.h>
@@ -9,7 +7,7 @@
 #include <stdlib.h>
 
 #define MAX_NAME_LENGTH 51
-#define MMAX_CATEGORY_LENGTH 20
+#define MAX_CATEGORY_LENGTH 20
 #define DBNAME "test.db"
 
 void restInput();
@@ -18,89 +16,30 @@ int isKoreanWhiteSpace(char c);
 void getRestName(char* name);
 void getRestRating(int* rating);
 void getRestCategory(char* category);
-int selectData(sqlite3* db);
+int insertData(sqlite3* db, char* restName, int rating, char* restCategory);
+
 
 void restInput() {
 	sqlite3* db = openDataBase(DBNAME);
-	int rc = sqlite3_open(DBNAME, &db);
 
 	char restName[MAX_NAME_LENGTH];
 	int rating;
-	char restCategory[20];
+	char restCategory[MAX_CATEGORY_LENGTH];
 
 	printf("새로운 식당 정보를 추가합니다!\n\n");
-	getRestName(restName);
+	getRestName(restName,db);
 	getRestRating(&rating);
 	getRestCategory(restCategory);
+	insertData(db, restName, rating, restCategory);
 
 
-	printf("이름: %s, 별점: %d, 분류: %s", restName, rating, restCategory);
-	
-	//db에 추가 ----------
-	char sql[300];
-	char* err_msg = NULL;
-	sprintf(sql, "INSERT INTO RESTAURANT (NAME, RATING, CATEGORY) VALUES('%s', '%d', '%s');", restName, rating, restCategory);
-	rc = sqlite3_exec(db, sql, 0, 0, &err_msg);
-
-	if (rc != SQLITE_OK)
-	{
-		fprintf(stderr, "SQL error: %s\n", err_msg);
-
-		sqlite3_free(err_msg);
-		sqlite3_close(db);
-		return 1;
-	}
-	//selectdata(db);
-
-
-
-	/// 데이터 조회 ------------------------------------------------
-	sqlite3_stmt* stmt;
-	const char* select_query = "SELECT * FROM restaurant;";
-
-	rc = sqlite3_prepare_v2(db, select_query, -1, &stmt, 0);
-
-	if (rc != SQLITE_OK) {
-		fprintf(stderr, "Failed to prepare statement: %s\n", sqlite3_errmsg(db));
-		return rc;
-	}
-
-	printf("restaurant\tNAME\tRATING\tCATEGORY\n");
-
-	while ((rc = sqlite3_step(stmt)) == SQLITE_ROW) {
-		int restaurant_no = sqlite3_column_int(stmt, 0);
-		const unsigned char* name = sqlite3_column_text(stmt, 1);
-		int rating = sqlite3_column_int(stmt, 2);
-		const unsigned char* category = sqlite3_column_text(stmt, 3);
-
-		printf("%d\t%s\t%d\t%s\n", restaurant_no, name, rating, category);
-	}
-
-	sqlite3_finalize(stmt);
-
-	if (rc != SQLITE_DONE) {
-		fprintf(stderr, "Failed to fetch data: %s\n", sqlite3_errmsg(db));
-		return rc;
-	}
-
-
-
-	//데이터 조회 --------------------------------
-
-	//sqlite3_close(db);
-
-
-
-
-
-
+	sqlite3_close(db);
 	return 0;
 }
 
 sqlite3* openDataBase(const char* filename) {
 	sqlite3* db;
 	char* err_msg = NULL;
-
 	int returnCode = sqlite3_open(filename, &db);
 
 	if (returnCode != SQLITE_OK) {
@@ -118,18 +57,18 @@ int isKoreanWhiteSpace(char c) {
 	return (c == 0x0020 || c == 0x00A0 || (c >= 0x2000 && c <= 0x200A) || c == 0x3000);
 }
 
-void getRestName(char* name) {
+void getRestName(char* name, sqlite3* db) {
 	char tmp[100];
 	int len;
 
 	while (1) {
 		printf("식당의 이름을 입력해주세요: ");
+		//memset(tmp, 0, sizeof(tmp));
 		fgets(tmp, sizeof(tmp), stdin);
-		//gets_s(tmp, sizeof(tmp));
 
 		tmp[strcspn(tmp, "\n")] = '\0';
+		fflush(stdin);
 
-		//���� �Է� ����
 		int validInput = 0;
 		for (int i = 0; tmp[i] != '\0'; i++) {
 			if (!isKoreanWhiteSpace(tmp[i])) {
@@ -153,41 +92,49 @@ void getRestName(char* name) {
 			printf("이름 '%s'이 맞나요? (y/n): ", tmp);
 			char answer = getchar();
 			while (getchar() != '\n');
-			//getchar(&answer);
-			//scanf("%c", &answer);
-			//getchar();
-
-			//��� Ȯ��
 
 			if (toupper(answer) == 'Y') {
 				strcpy(name, tmp);
+
+				//------
+				char query[100];
+				sprintf(query, "SELECT NAME FROM RESTAURANT WHERE NAME = '%s'", name);
+				sqlite3_stmt* stmt;
+				int rc = sqlite3_prepare_v2(db, query, -1, &stmt, NULL);
+				if (rc != SQLITE_OK) {
+					printf("쿼리 준비 중 오류가 발생했습니다.\n");
+					continue;
+				}
+				// 결과 확인
+				rc = sqlite3_step(stmt);
+				if (rc == SQLITE_ROW) {
+					printf("이미 존재하는 식당입니다.\n");
+					break;
+				}
+				sqlite3_finalize(stmt);
 				return;
-			}
-			else if (toupper(answer) == 'N') {
+			} else if (toupper(answer) == 'N') {
 				break;
-			}
-			else {
+			} else {
 				printf("잘못된 입력입니다.. y/n 중에 입력해주세요.\n");
 				continue;
 			}
 		}
-
 	}
 }
 
 void getRestRating(int* rating) {
-	//char tmp[100]; 
-	int input;
+	int tmp;
 
 	while (1) {
 		printf("식당의 별점을 0점에서 5점 사이로 입력해주세요!: ");
 
-		if (scanf("%d", &input) != 1 || input < 0 || input >5) {
+		if (scanf("%d", &tmp) != 1 || tmp < 0 || tmp >5) {
 			printf("입력이 잘못되었습니다. 0 이상 5 이하의 값을 입력해주세요.\n"); // 한글 부분 수정
 			while (getchar() != '\n');
 		}
 		else {
-			*rating = input;
+			*rating = tmp;
 			break;
 		}
 	}
@@ -223,34 +170,19 @@ void getRestCategory(char* category) {
 	}
 }
 
-int selectData(sqlite3* db) {
-	sqlite3_stmt* stmt;
-	const char* select_query = "SELECT * FROM restaurant;";
+int insertData(sqlite3* db, char* restName, int rating, char* restCategory) {
+	char sql[150];
+	char* err_msg = NULL;
+	sprintf(sql, "INSERT INTO RESTAURANT (NAME, RATING, CATEGORY) VALUES('%s', '%d', '%s');", restName, rating, restCategory);
+	int rc = sqlite3_exec(db, sql, 0, 0, &err_msg);
 
-	int rc = sqlite3_prepare_v2(db, select_query, -1, &stmt, 0);
+	if (rc != SQLITE_OK)
+	{
+		fprintf(stderr, "SQL error: %s\n", err_msg);
 
-	if (rc != SQLITE_OK) {
-		fprintf(stderr, "Failed to prepare statement: %s\n", sqlite3_errmsg(db));
-		return rc;
+		sqlite3_free(err_msg);
+		//sqlite3_close(db);
+		return 1;
 	}
-
-	printf("restaurant\tNAME\tRATING\tCATEGORY\n");
-
-	while ((rc = sqlite3_step(stmt)) == SQLITE_ROW) {
-		int restaurant_no = sqlite3_column_int(stmt, 0);
-		const unsigned char* name = sqlite3_column_text(stmt, 1);
-		int rating = sqlite3_column_int(stmt, 2);
-		const unsigned char* category = sqlite3_column_text(stmt, 3);
-
-		printf("%d\t%s\t%d\t%s\n", restaurant_no, name, rating, category);
-	}
-
-	sqlite3_finalize(stmt);
-
-	if (rc != SQLITE_DONE) {
-		fprintf(stderr, "Failed to fetch data: %s\n", sqlite3_errmsg(db));
-		return rc;
-	}
-
-	return SQLITE_OK;
+	printf("이름: %s, 별점: %d, 분류: %s 이 잘 추가되었습니다! \n\n", restName, rating, restCategory);
 }
