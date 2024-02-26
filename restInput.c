3d9fb119cd80287e310a1cd1dc5bb2ecdd8f05dd
Toughie17 <airj907@gmail.com>
2024-02-26 18:56:20 +0900

    feat: restInput 거리 및 주소 입력 추가

--- restInput.c
+++ restInput.c
@@ -10,6 +10,7 @@
 
 #define MAX_NAME_LENGTH 51
 #define MAX_CATEGORY_LENGTH 20
+#define MAX_ADDRESS_LENGTH 300
 #define DBNAME "test.db" //추후 사용할 db의 이름으로 변경하면 됩니다. (csv 로드 할 db)
 
 void restInput(); //아래 함수들을 호출하는 부모 함수.
@@ -18,7 +19,9 @@ int isKoreanWhiteSpace(char c); // 공백 입력 방지, 공백 체크 함수
 void getRestName(char* name);// 식당의 이름을 입력받습니다. (공백, 길이, 입력 재확인, db중복확인)
 void getRestRating(int* rating);//평점을 입력받습니다.(0~5점 입력 체크)
 void getRestCategory(char* category);//분류를 입력받습니다.(정수형 입력)
-int insertData(sqlite3* db, char* restName, int rating, char* restCategory);//입력값들을 통해 테이블에 행을 추가합니다.
+void getDistance(int* distance);
+void getAddress(char* address);
+int insertData(sqlite3* db, char* restName, int rating, char* restCategory, int distance, char* address);//입력값들을 통해 테이블에 행을 추가합니다.
 
 
 void restInput() {
@@ -27,12 +30,16 @@ void restInput() {
 	char restName[MAX_NAME_LENGTH];
 	int rating;
 	char restCategory[MAX_CATEGORY_LENGTH];
+	int distance;
+	char address[MAX_ADDRESS_LENGTH];
 
 	printf("새로운 식당 정보를 추가합니다!\n\n");
 	getRestName(restName, db);
 	getRestRating(&rating);
 	getRestCategory(restCategory);
-	insertData(db, restName, rating, restCategory);
+	getDistance(&distance);
+	getAddress(address);
+	insertData(db, restName, rating, restCategory, distance, address);
 
 	sqlite3_close(db);
 	return 0;
@@ -166,10 +173,59 @@ void getRestCategory(char* category) {
 	}
 }
 
-int insertData(sqlite3* db, char* restName, int rating, char* restCategory) {
-	char sql[150];
+void getDistance(int* distance) {
+	int tmp;
+
+	while (1) {
+		printf("식당의 거리를 600미터 이하로 입력해 주세요: ");
+
+		if (scanf("%d", &tmp) != 1 || tmp < 0 || tmp >600) {
+			printf("\t입력이 잘못되었습니다. 0 이상 600 이하의 값을 입력해주세요.\n\n");
+			while (getchar() != '\n');
+		}
+		else {
+			*distance = tmp;
+			break;
+		}
+	}
+	while (getchar() != '\n');
+}
+
+void getAddress(char* address) {
+	char tmp[500];
+	while (1) {
+		printf("식당의 주소를 입력해 주세요: ");
+		memset(tmp, 0, sizeof(tmp));
+		fflush(stdin);
+
+		fgets(tmp, sizeof(tmp), stdin);
+		tmp[strcspn(tmp, "\n")] = '\0';
+
+		int validInput = 0;
+		for (int i = 0; tmp[i] != '\0'; i++) {
+			if (!isKoreanWhiteSpace(tmp[i])) {
+				validInput = 1;
+				break;
+			}
+		}
+		if (!validInput) {
+			printf("\t공백을 입력하셨습니다. 잘못된 입력입니다.\n\n");
+			continue;
+		}
+
+		if (strlen(tmp) > MAX_ADDRESS_LENGTH - 1) {
+			printf("\t주소가 너무 깁니다. %d자 이내로 입력해 주세요.\n\n", (MAX_ADDRESS_LENGTH - 1));
+			continue;
+		}
+		strcpy(address, tmp);
+		break;
+	}
+}
+
+int insertData(sqlite3* db, char* restName, int rating, char* restCategory, int distance, char* address) {
+	char sql[500];
 	char* err_msg = NULL;
-	sprintf(sql, "INSERT INTO RESTAURANT (NAME, RATING_SUM, CATEGORY, PEOPLE, RATING_AVG) VALUES('%s', '%d', '%s','%d','%lf');", restName, rating, restCategory,1,(double)rating);
+	sprintf(sql, "INSERT INTO RESTAURANT (NAME, RATING_SUM, CATEGORY, PEOPLE, RATING_AVG, DISTANCE, ADDRESS) VALUES('%s', '%d', '%s','%d','%lf', '%d', '%s');", restName, rating, restCategory, 1, (double)rating, distance, address);
 	int rc = sqlite3_exec(db, sql, 0, 0, &err_msg);
 
 	if (rc != SQLITE_OK)
